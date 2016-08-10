cc60dd1f28a9b8ab1f820e7e0f9323c10b8b5ed1
TheCoub <ryan.courbron@baxter-academy.org>
2016-08-10 15:49:03 -0400

    Initial Commit
    This is my first Commit

---
+++ Assets/Movement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public Rigidbody2D phys;
+	public int jumpSpeed = 10;
+
+	// Use this for initialization
+	void Start () {
+		phys = GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		jump ();
+	}
+
+	public void jump(){
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			phys.velocity = Vector2.up * jumpSpeed;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D collision){
+		Destroy(collision.gameObject);
+	}
+}
